37df6659148be2832fa0c0f0b42b3892f89a1842
Yash Choudhary <yc1998a@gmail.com>
2020-04-05 02:43:45 +0530

    Minor changes + Dark Mode

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,22 @@
     Description: This file contains all the scripts associated with the single-page
     portfolio website.
 */
+var options = {
+  bottom: '64px', // default: '32px'
+  right: '32px', // default: '32px'
+  left: 'unset', // default: 'unset'
+  time: '0.5s', // default: '0.3s'
+  mixColor: '#fff', // default: '#fff'
+  backgroundColor: '#fff',  // default: '#fff'
+  buttonColorDark: '#272641',  // default: '#100f2c'
+  buttonColorLight: '#fff', // default: '#fff'
+  saveInCookies: false, // default: true,
+  label: '🌓', // default: ''
+  autoMatchOsTheme: true // default: true
+}
+
+const darkmode = new Darkmode(options);
+darkmode.showWidget();
 
 (function($) {
 
